51ea1c2b0583ddb34d5f0fe6d25e140c8764e24a
jmonroym@outlook.cl <jmonroy@PETER-PC>
2025-03-26 21:36:20 -0300

    pagination working

--- amazon_jobs_scrapy/amazon_jobs_scrapy/spiders/amazon_jobs.py
+++ amazon_jobs_scrapy/amazon_jobs_scrapy/spiders/amazon_jobs.py
@@ -1,6 +1,5 @@
 import scrapy
 from scrapy_playwright.page import PageMethod
-#from playwright.async_api import Page
 
 class AmazonJobsSpider(scrapy.Spider):
     name = "amazon_jobs"
@@ -23,7 +22,6 @@ class AmazonJobsSpider(scrapy.Spider):
 
     async def parse(self, response):
         page = response.meta["playwright_page"]
-        #await page.close()
         headers = response.xpath("//div[@role=\"button\"]//h3")
         metadata = response.xpath("//div[@role=\"button\"]//div[contains(@class, \"metadata-wrapper\")]")
         bodies = response.xpath(".//div[@role=\"button\"]/div/div[2]/div")
@@ -36,12 +34,22 @@ class AmazonJobsSpider(scrapy.Spider):
                 "description_snippet": body.xpath("./text()").getall(),
             }
 
-        next_button = await page.query_selector("//button[@data-test-id='next-page']")
+        next_button = page.locator("//button[@data-test-id='next-page']")
         if await next_button.is_visible():
             self.logger.info("Paginación encontrada. Pasando a la siguiente página sin recargar...")
-            
+
+            last_job_title = header.xpath("./a/text()").get()
+
             await next_button.click()
-            await page.wait_for_selector("//div[@role='button']", state="visible")
+            await page.wait_for_timeout(2 * 1000)
+
+            await page.wait_for_function(
+                """(lastTitle) => {
+                    const jobLinks = document.querySelectorAll('div[role="button"] h3 a');
+                    return jobLinks.length > 0 && jobLinks[jobLinks.length - 1].textContent.trim() !== lastTitle;
+                }""",
+                arg=last_job_title
+            )
 
             new_html = await page.content()
             new_response = response.replace(body=new_html, encoding="utf-8")
@@ -51,31 +59,15 @@ class AmazonJobsSpider(scrapy.Spider):
 
         await page.close()
 
-        # next_button = response.xpath("//button[@data-test-id='next-page']")
-        # if next_button:
-        #     self.logger.info("Paginación encontrada. Pasando a la siguiente página...")
-        #     yield scrapy.Request(
-        #         url=response.url,
-        #         dont_filter=True,
-        #         meta={
-        #             "playwright": True,
-        #             "playwright_include_page": True,
-        #             "playwright_context": "default",
-        #             "playwright_page_methods": [
-        #                 PageMethod("wait_for_selector", "//button[@data-test-id='next-page']", state="visible"),
-        #                 PageMethod("evaluate", "window.scrollBy(0, document.body.scrollHeight)"),
-        #                 #PageMethod("click", "//button[@data-test-id='next-page']"),
-        #                 PageMethod("evaluate", "document.querySelector('[data-test-id=\"next-page\"]').click()"),
-        #                 PageMethod("wait_for_selector", "//ul[contains(@class, 'jobs-module_root')]/li[10]"),
-        #                 #PageMethod("wait_for_timeout", 45 * 1000),
-        #             ],
-        #             "errback": self.errback,
-        #         },
-        #         callback=self.parse,  # Vuelve a ejecutar parse con la nueva página
-        #     )
+    async def details_parse(self, response):
+        page = response.meta["playwright_page"]
+        yield scrapy.Request(
+            url="https://httpbin.org/headers",
+            callback=self.parse_headers,
+            meta={"playwright": True, "playwright_page": page},
+        )
+        await page.close()
 
-        
-        
     async def errback(self, failure):
         page = failure.request.meta.get("playwright_page")
         if page:
